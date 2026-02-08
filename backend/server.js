a77e33ef963c5d31af20be2d6a4a086ce316ac34
vedantkhedkar <vedantkhedkar4@gmail.com>
2026-02-08 21:36:00 +0530

    updated vercel url

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const app = express();
 // --- MIDDLEWARES ---
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://projecthub-backend-ten.vercel.app"],
     credentials: true,
   })
 );
