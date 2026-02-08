33a4e441ee629b4d566582a9a4555422a88a6b25
vedantkhedkar <vedantkhedkar4@gmail.com>
2026-02-08 21:43:22 +0530

    again server update

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const app = express();
 // --- MIDDLEWARES ---
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://projecthub-backend-ten.vercel.app"],
+    origin: ["http://localhost:5173", "https://projecthub-frontend-five.vercel.app"],
     credentials: true,
   })
 );
